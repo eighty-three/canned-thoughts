afc4d453ece36b8bc6c86c8dda3f8841cfb7d1cf
Eighty Three <cwp1@protonmail.com>
2020-09-02 07:32:08 +0800

    Fix type from string to object with string as key

--- api/src/account/accountModel.ts
+++ api/src/account/accountModel.ts
@@ -1,6 +1,14 @@
 import db from '@utils/db';
 const accountsTable = 'accounts';
 
+interface IUsername {
+  username: string;
+}
+
+interface IPassword {
+  password: string;
+}
+
 export const createAccount = async (
   username: string, 
   hash: string, 
@@ -15,7 +23,7 @@ export const createAccount = async (
 export const checkUsername = async (
   username: string, 
   argAccountsTable: string = accountsTable
-): Promise<string | null> => {
+): Promise<IUsername | null> => {
   return await db.oneOrNone(
     'SELECT username FROM $1:name WHERE username=$2 \
     ', [argAccountsTable, username]);
@@ -24,7 +32,7 @@ export const checkUsername = async (
 export const checkPassword = async (
   username: string, 
   argAccountsTable: string = accountsTable
-): Promise<string | null> => {
+): Promise<IPassword | null> => {
   return await db.oneOrNone(
     'SELECT password FROM $1:name WHERE username=$2 \
     ', [argAccountsTable, username]);
