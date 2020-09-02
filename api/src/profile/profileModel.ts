9ee7bb0318039dd4e208b1f56375cb4ec83cc31d
Eighty Three <cwp1@protonmail.com>
2020-09-02 12:54:35 +0800

    Add types for return values of functions

--- api/src/profile/profileModel.ts
+++ api/src/profile/profileModel.ts
@@ -1,10 +1,18 @@
 import db from '@utils/db';
 const accountsTable = 'accounts';
 
+interface IProfileInfo {
+  name: string,
+  description: string,
+  followers: number
+}
+
+type INameAndDescription = Omit<IProfileInfo, 'followers'>;
+
 export const getProfileInfo = async (
   username: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<IProfileInfo> => {
   return await db.one(
     'SELECT name, description, followers FROM $1:name WHERE username=$2 \
     ', [argAccountsTable, username]);
@@ -13,7 +21,7 @@ export const getProfileInfo = async (
 export const getNameAndDescription = async (
   username: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<INameAndDescription> => {
   return await db.one(
     'SELECT name, description FROM $1:name WHERE username=$2 \
     ', [argAccountsTable, username]);
@@ -24,7 +32,7 @@ export const updateNameAndDescription = async (
   newName: string, 
   newDescription: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<void> => {
   await db.none(
     'UPDATE $1:name \
     SET name=$2, description=$3 \
