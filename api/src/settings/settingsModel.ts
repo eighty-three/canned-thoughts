a9cdff8fc4f36fccb8d1d9c08e8a0415a2d02d7f
Eighty Three <cwp1@protonmail.com>
2020-09-02 13:23:52 +0800

    Add types for return values of functions

--- api/src/settings/settingsModel.ts
+++ api/src/settings/settingsModel.ts
@@ -5,7 +5,7 @@ export const changePassword = async (
   username: string, 
   hash: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<void> => {
   await db.none(
     'UPDATE $1:name SET password=$2 WHERE username=$3 \
     ', [argAccountsTable, hash, username]);
@@ -15,7 +15,7 @@ export const changeUsername = async (
   username: string, 
   newUsername: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<void> => {
   await db.none(
     'UPDATE $1:name SET username=$3 WHERE username=$2 \
     ', [argAccountsTable, username, newUsername]);
@@ -24,7 +24,7 @@ export const changeUsername = async (
 export const deleteAccount = async (
   username: string, 
   argAccountsTable: string = accountsTable
-) => {
+): Promise<void> => {
   await db.none(
     'DELETE FROM $1:name WHERE username=$2 \
     ', [argAccountsTable, username]);
