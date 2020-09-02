3df6332a38f58c454054f2c8d3af07d9ad195487
Eighty Three <cwp1@protonmail.com>
2020-09-02 08:51:07 +0800

    Add validator

--- api/src/account/accountRouter.ts
+++ api/src/account/accountRouter.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import validator from '@utils/validator';
 const router = express.Router();
 
 import { authToken, authAccount } from '@authMiddleware/index';
 
 import * as account from './accountController';
+import * as accountSchema from './account.schema';
 const accountRoutes = ['/login', '/signup'];
+
 router.post(accountRoutes, authToken.verifyExistingToken);
-router.post('/login', authAccount.checkPassword, account.login);
-router.post('/signup', authAccount.checkExistingUsername, account.signup);
-router.post('/logout', account.logout);
+router.post('/login', validator(accountSchema.login, 'body'), authAccount.checkPassword, account.login);
+router.post('/signup', validator(accountSchema.signup, 'body'), authAccount.checkExistingUsername, account.signup);
+router.post('/logout', validator(accountSchema.logout, 'body'), account.logout);
 
 export default router;
