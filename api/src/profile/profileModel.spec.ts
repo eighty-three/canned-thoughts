629e5eb5b43eddf7d615f646971c5f3f964014f0
Eighty Three <cwp1@protonmail.com>
2020-09-02 12:54:42 +0800

    Add tests for profileModel.ts

---
+++ api/src/profile/profileModel.spec.ts
@@ -0,0 +1,50 @@
+import * as account from '../account/accountModel';
+import * as settings from '../settings/settingsModel';
+import * as profile from './profileModel';
+const testTable = 'accounts_test';
+
+beforeAll(async () => {
+  await account.createAccount('dummy1', 'pw1', testTable);
+});
+
+afterAll(async () => {
+  await settings.deleteAccount('dummy1', testTable);
+});
+
+describe('testing functions', () => {
+  test('getProfileInfo', async () => {
+    expect(await profile.getProfileInfo('dummy1', testTable)).toStrictEqual({
+      name: 'dummy1',
+      description: null,
+      followers: 0
+    });
+  });
+
+  test('getNameAndDescription', async () => {
+    expect(await profile.getNameAndDescription('dummy1', testTable)).toStrictEqual({
+      name: 'dummy1',
+      description: null
+    });
+  });
+
+  test('updateNameAndDescription', async () => {
+    expect(await profile.getNameAndDescription('dummy1', testTable)).toStrictEqual({
+      name: 'dummy1',
+      description: null
+    });
+
+    await profile.updateNameAndDescription('dummy1', 'newDummy', 'New Description', testTable);
+
+    expect(await profile.getNameAndDescription('dummy1', testTable)).toStrictEqual({
+      name: 'newDummy',
+      description: 'New Description'
+    });
+
+    await profile.updateNameAndDescription('dummy1', 'backToDummy1', 'Old Description', testTable);
+
+    expect(await profile.getNameAndDescription('dummy1', testTable)).toStrictEqual({
+      name: 'backToDummy1',
+      description: 'Old Description'
+    });
+  });
+});
