48ed7b8dcfc049e10feabe6d37e3d3d263bf3dda
Eighty Three <cwp1@protonmail.com>
2020-09-04 21:02:23 +0800

    Add tests for posts functions

---
+++ api/src/content/contentModel.spec.ts
@@ -0,0 +1,70 @@
+import * as content from './contentModel';
+import * as account from '../account/accountModel';
+
+beforeAll(async () => {
+  await account.createAccount('dummy', '123');
+});
+
+describe('testing posts', () => {
+  test('createPost with tags', async () => {
+    const tags = ['1', '2', '3'];
+    expect(await content.createPost('dummy', 'post', 'url1', tags)).toStrictEqual({post_id: 1});
+  });
+
+  test('createPost with no tags', async () => {
+    expect(await content.createPost('dummy', 'post', 'url2')).toStrictEqual({post_id: 2});
+  });
+
+  test('getPost with tags', async () => {
+    expect(await content.getPost('url1')).toMatchObject({
+      post: 'post',
+      url: 'url1',
+      tags: '1|2|3'
+    });
+  });
+
+  test('getPost with no tags', async () => {
+    expect(await content.getPost('url2')).toMatchObject({
+      post: 'post',
+      url: 'url2',
+      tags: null
+    });
+  });
+
+  test('getPosts offset', async () => {
+    await content.createPost('dummy', 'post', 'url3');
+
+    expect(await content.getPosts('dummy', 0)).toHaveLength(3);
+    expect(await content.getPosts('dummy', 1)).toHaveLength(2);
+  });
+
+  test('getPosts contains correct data', async () => {
+    expect(await content.getPosts('dummy', 1)).toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        post: 'post',
+        tags: null,
+        url: 'url3'
+      })
+    ]));
+
+    expect(await content.getPosts('dummy', 1)).not.toEqual(expect.arrayContaining([
+      expect.objectContaining({
+        post: 'post',
+        tags: '1|2|3',
+        url: 'url1'
+      })
+    ]));
+  });
+
+  test('deletePost', async () => {
+    expect(await content.getPosts('dummy', 0)).toHaveLength(3);
+
+    await content.deletePost('url1');
+
+    expect(await content.getPosts('dummy', 0)).toHaveLength(2);
+
+    await content.deletePost('url2');
+
+    expect(await content.getPosts('dummy', 0)).toHaveLength(1);
+  });
+});
