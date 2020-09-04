3e394c4ddcf967eb7523063b06c8fe695f9fdc21
Eighty Three <cwp1@protonmail.com>
2020-09-04 19:07:41 +0800

    Separate resetting sequence and deleting data

--- api/src/utils/dbSetup.ts
+++ api/src/utils/dbSetup.ts
@@ -14,11 +14,13 @@ const serials = [
   { table: 'tags', serialColumn: 'tag_id' },
 ];
 
-export const resetDatabase = async (): Promise<void> => {
+export const resetData = async (): Promise<void> => {
   tables.forEach(async (table) => {
     await db.none('DELETE FROM $1:name', [table]);
   });
-  
+};
+
+export const resetSequence = async (): Promise<void> => {
   // await db.none('ALTER SEQUENCE <tablename>_<id>_seq RESTART;');
   serials.forEach(async (obj) => {
     const input = `${obj.table}_${obj.serialColumn}_seq`;
