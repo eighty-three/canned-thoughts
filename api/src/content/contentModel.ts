5439143140cacc8126316d69e0310dd8d4f6d433
Eighty Three <cwp1@protonmail.com>
2020-09-05 18:07:40 +0800

    Separate types, fix createPost function
    I wanted to try optimizing the whole function chain
    when creating a post. First, it needs to create the
    post itself, sending the necessary data for the
    INSERT query. Afterwards, if there are tags present,
    it needs to create a new tag for each (up to 3). Lastly,
    it needs the post_id and tag_id, both returned from their
    respective queries. Apparently, simply looping through them
    is a terrible idea according to the `pg-promise` FAQ. And so,
    initially, I wanted to use the task method and the tx method
    together. However, it keeps on giving me errors. What I wanted
    was to go through the whole chain in one request:
    INSERT -> SELECT -> INSERT
    Unfortunately, it kept on spitting out BatchErrors/DatabaseErrors:
    "current transaction is aborted, commands ignored etc. etc."
    which apparently means that there was a fuckup along the way
    so it decided to shut itself down. I didn't know how to fix it;
    it probably had to do something with race conditions or something.
    It probably would have worked if I tried turning it into a single
    query like what the author said in the FAQ but I couldn't figure it out.
    I was able to create (aka copy an answer) an INSERT -> SELECT
    query but an INSERT -> SELECT -> INSERT query was too much for me.
    So I had to make do with 3 connections in a single function call:
    1. creating the post (returning post id)
    2. creating/checking the tags* (returning tag_id)
    3. creating the post/tag relation
    *The tags are in one tx so that's something positive at least
    It's most likely some garbage optimization but at least it works;
    the tests aren't shitting on me anymore, at least for now.

--- api/src/content/contentModel.ts
+++ api/src/content/contentModel.ts
@@ -1,41 +1,76 @@
 import db from '@utils/db';
 import { PreparedStatement as PS } from 'pg-promise';
+import { IPost, IRelation, IOptions } from './content.types';
 
 const accountsTable = 'accounts';
 const postsTable = 'posts';
 const postsTagsTable = 'posts_tags';
-
-interface IPost {
-  post: string;
-  url: string;
-  date: number;
-  tags: string;
-}
 
 export const createPost = async (
   username: string,
   post: string,
   url: string,
   tags?: string[]
-): Promise<number> => {
-  /* From [ 'tag1', 'tag2', 'tag3' ] as an array
-   * to 'tag1|tag2|tag3' as a string
-   * since the | character won't be allowed in the form
-   * so when getting the post data, I can easily transform it
-   * with a split call using the | character
-   */
-  const fixedTags = (tags)
-    ? `${tags.join('|')}`
-    : null;
-
-  const query = new PS({ name: 'create-post', text: '\
-    INSERT INTO posts (user_id, post, url, tags) \
-    VALUES ((SELECT user_id FROM accounts WHERE username=$1), $2, $3, $4) \
-    RETURNING post_id',
-  });
-
-  query.values = [username, post, url, fixedTags];
-  return await db.one(query);
+): Promise<void> => {
+  if (tags) {
+    /* From [ 'tag1', 'tag2', 'tag3' ] as an array
+     * to 'tag1|tag2|tag3' as a string
+     * since the | character won't be allowed in the form
+     * so when getting the post data, I can easily transform it
+     * with a split call using the | character
+     */
+    const fixedTags = `${tags.join('|')}`;
+    const createPost = new PS({ name: 'create-post-with-tags', text: '\
+      INSERT INTO posts (user_id, post, url, tags) \
+      VALUES ((SELECT user_id FROM accounts WHERE username=$1), $2, $3, $4) \
+      RETURNING post_id',
+    });
+    
+    createPost.values = [username, post, url, fixedTags];
+    const createPostOutput = await db.one(createPost);
+    
+    const tag_ids = await db.tx(t => {
+      const tagQueries = tags.map((tag) => {
+        const tagQuery = new PS({ name: 'create-tag', text: '\
+          WITH ins AS (\
+            INSERT INTO tags (tag_name)\
+            VALUES ($1)\
+            ON     CONFLICT (tag_name) DO UPDATE\
+            SET    tag_name = NULL \
+            WHERE  FALSE\
+            RETURNING tag_id\
+            )\
+          SELECT tag_id FROM ins\
+            UNION  ALL\
+            SELECT tag_id FROM tags\
+            WHERE  tag_name = $1'
+        });
+    
+        tagQuery.values = [tag];
+        return t.one(tagQuery);
+      });
+    
+      return t.batch(tagQueries);
+    });
+    
+    await db.tx(t => {
+      const queries = tag_ids.map((tag) => {
+        t.none(
+          'INSERT INTO posts_tags (post_id, tag_id) \
+          VALUES ($1, $2)', [createPostOutput.post_id, tag.tag_id]);
+      });
+    
+      return t.batch(queries);
+    });
+  } else { // If no tags
+    const query = new PS({ name: 'create-post-no-tags', text: '\
+      INSERT INTO posts (user_id, post, url) \
+      VALUES ((SELECT user_id FROM accounts WHERE username=$1), $2, $3)'
+    });
+    
+    query.values = [username, post, url];
+    await db.none(query);
+  }
 };
 
 export const getPost = async (
@@ -80,7 +115,7 @@ export const deletePost = async (
 // Tags
 export const getTag = async (
   tag: string
-): Promise<number> => {
+): Promise<number|void> => {
   // https://stackoverflow.com/a/40325406/435563
   // tl;dr: inefficiencies and race conditions
   const query = new PS({ name: 'get-tag', text: '\
@@ -113,22 +148,10 @@ export const deleteTag = async (
   await db.none(query);
 };
 
-export const createPostTagRelation = async (
-  post_id:number,
-  tag_id: number
-): Promise<void> => {
-  const query = new PS({ name: 'create-post-tag-relation', text: '\
-    INSERT INTO posts_tags (post_id, tag_id) VALUES ($1, $2)'
-  });
 
-  query.values = [post_id, tag_id];
-  await db.none(query);
+export const checkPostsTagsTable = async (): Promise<IRelation[] | null> => {
+  return await db.manyOrNone('SELECT * FROM posts_tags');
 };
-
-interface IOptions {
-  scope: 'inclusive' | 'exclusive';
-  followedOnly: boolean;
-}
 
 // To do
 export const searchPostsWithTag = async (
