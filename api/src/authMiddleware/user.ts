a1342be6bd66514eec763d194a09c61c3a53d593
Eighty Three <cwp1@protonmail.com>
2020-09-07 12:26:26 +0800

    Fix import

--- api/src/authMiddleware/user.ts
+++ api/src/authMiddleware/user.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from 'express';
-import * as account from '../account/model';
+import * as authAccount from './accountModel';
 
 export const authCheck: RequestHandler = async (req, res) => {
   const { username } = req.body;
 
-  const user = await account.checkUsername(username);
+  const user = await authAccount.checkUsername(username);
   if (!user) {
     res.json({error: 'Profile not found'});
     return;
