857ec2798b58b69a0c3053f74880e86683d33014
Eighty Three <cwp1@protonmail.com>
2020-09-08 22:53:49 +0800

    Add username and name to IPost

--- api/src/content/types.ts
+++ api/src/content/types.ts
@@ -1,8 +1,10 @@
 export interface IPost {
+  username: string;
+  name: string;
   post: string;
   url: string;
   date: number;
-  tags: string;
+  tags?: string|null
 }
 
 export interface IRelation {
