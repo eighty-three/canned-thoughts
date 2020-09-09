fc5a4b090ae69c8c08ec055e52491912c1182bfd
Eighty Three <cwp1@protonmail.com>
2020-09-10 02:00:35 +0800

    Change newDescription to optional

--- api/src/profile/schema.ts
+++ api/src/profile/schema.ts
@@ -2,8 +2,8 @@ import Joi from '@hapi/joi';
 
 export const updateNameAndDescription = Joi.object({
   username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required(),
-  newName: Joi.string().regex(/^[a-zA-Z0-9 _$!@?#-]{1,49}$/).required(),
-  newDescription: Joi.string().regex(/^[a-zA-Z0-9 _$!@?#-]{1,159}$/).required()
+  newName: Joi.string().regex(/^[a-zA-Z0-9 _$!@?#.-]{1,49}$/).required(),
+  newDescription: Joi.string().regex(/^[\\/\[\]\(\) a-zA-Z0-9 _'":;~%^&*$!@?#,.-]{1,149}$/).optional()
 });
 
 export const getNameAndDescription = Joi.object({
