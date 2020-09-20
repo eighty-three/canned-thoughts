55796e127598985bef1f44d0d7344f04ed60adfe
Eighty Three <cwp1@protonmail.com>
2020-09-20 15:19:36 +0800

    Remove all functions except toggleFollowStatus
    The functions 'checkIfFollowed' and 'getFollowersCount' were merged
    into getProfileInfo. Also fixed errorhandling of 'toggleFollowStatus'

--- client/src/lib/follows.js
+++ client/src/lib/follows.js
@@ -2,42 +2,17 @@ import ky from 'ky-universal';
 import HOST from '@/lib/host';
 const api = `${HOST}/api/follows`;
 
-export async function toggleFollowStatus(username, profileUsername) {
-  const request = await ky.post(`${api}/toggle`,
-    {
-      json:
-        {
-          followerUsername: username,
-          followedUsername: profileUsername
-        }
-    });
-  const response = await request.json();
-  return response;
-}
+export const toggleFollowStatus = async (username, profileUsername) => {
+  const queryData = {
+    followerUsername: username,
+    followedUsername: profileUsername
+  };
 
-export async function checkIfFollowed(username, profileUsername) {
-  const request = await ky.post(`${api}/check`,
-    {
-      json:
-        {
-          followerUsername: username,
-          followedUsername: profileUsername
-        }
-    });
-  const response = await request.json();
-  return response;
-}
-
-export async function getFollowersCount(profileUsername) {
-  const request = await ky.post(`${api}/count`,
-    {
-      json:
-        {
-          profileUsername
-        }
-    });
-
-  const response = await request.json();
-  return response.followers;
-}
-
+  try {
+    const req = await ky.post(`${api}/toggle`, { json: queryData });
+    const response = await req.json();
+    return response;
+  } catch (err) {
+    return { error: 'Something went wrong' };
+  }
+};
