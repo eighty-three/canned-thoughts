93247ea1409e5e0048f02320785732a8c9cfb1b9
Eighty Three <cwp1@protonmail.com>
2020-09-20 15:34:35 +0800

    Fix indentation

--- client/src/components/CustomAuthForms/transForm.js
+++ client/src/components/CustomAuthForms/transForm.js
@@ -1,13 +1,13 @@
 const transForm = (formType) => {
   switch(formType) {
-  case 'username':
-    return { id: formType, label: 'Username', username: true };
-  case 'newUsername':
-    return { id: formType, label: 'New Username', username: true };
-  case 'password':
-    return { id: formType, label: 'Password', username: false };
-  case 'newPassword':
-    return { id: formType, label: 'New Password', username: false };
+    case 'username':
+      return { id: formType, label: 'Username', username: true };
+    case 'newUsername':
+      return { id: formType, label: 'New Username', username: true };
+    case 'password':
+      return { id: formType, label: 'Password', username: false };
+    case 'newPassword':
+      return { id: formType, label: 'New Password', username: false };
   }
 };
 
