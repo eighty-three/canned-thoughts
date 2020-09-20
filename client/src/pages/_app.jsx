2a3b7e7d1e3dab7637a0ce76e18a73b2f161c138
Eighty Three <cwp1@protonmail.com>
2020-09-20 15:29:49 +0800

    Add prop types

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import '@/styles/global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Router from 'next/router';
 
 //NProgress
@@ -10,6 +11,15 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function App({ Component, pageProps }) {
+const propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object
+};
+
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
+
+App.propTypes = propTypes;
+
+export default App;
