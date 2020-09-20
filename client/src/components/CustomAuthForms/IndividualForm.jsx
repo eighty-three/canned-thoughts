756296656561e9a788037f18df52fece4f9c27c8
Eighty Three <cwp1@protonmail.com>
2020-09-20 15:35:36 +0800

    Add prop types and allow uppercase characters for username

--- client/src/components/CustomAuthForms/IndividualForm.jsx
+++ client/src/components/CustomAuthForms/IndividualForm.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Form from 'react-bootstrap/Form';
 
-export default function IndividualForm({ id, label, username, title, register }) {
+const propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  username: PropTypes.bool,
+  title: PropTypes.string,
+  register: PropTypes.func
+};
+
+const IndividualForm = (props) => {
+  const {
+    id,
+    label,
+    username,
+    title,
+    register
+  } = props;
+
   return (
     <Form.Group controlId={`${title} ${id}`}>
       <Form.Label>{label}:</Form.Label>
@@ -11,7 +28,7 @@ export default function IndividualForm({ id, label, username, title, register })
           <Form.Control 
             type="text"
             maxLength={30}
-            pattern="[a-z0-9_]{1,29}"
+            pattern="[a-zA-Z0-9_]{1,29}"
             placeholder="[ a-z0-9_ ]{1,29}"
             spellCheck="false"
             aria-describedby={id}
@@ -31,5 +48,8 @@ export default function IndividualForm({ id, label, username, title, register })
       }
     </Form.Group>
   );
-}
+};
 
+IndividualForm.propTypes = propTypes;
+
+export default IndividualForm;
