af8c2fb67dc83459d815f462e758b789f5ffde47
Eighty Three <cwp1@protonmail.com>
2020-09-20 20:29:13 +0800

    Remove case 'first'

--- client/src/lib/PaginationReducer.js
+++ client/src/lib/PaginationReducer.js
@@ -1,18 +1,5 @@
 const PaginationReducer = (state, action) => {
   switch (action.type) {
-    case 'first': {
-      // If the query is more than 10, it means that there should be a next page
-      // because the display is only for 10 posts.
-      const nextButtonState = (action.payload.length > 10) ? false : true;
-
-      return { 
-        page: 1,
-        posts: action.payload,
-        prevButtonDisabled: true,
-        nextButtonDisabled: nextButtonState
-      };
-    }
-
     case 'update': {
       // If the page before the incoming page is the last one (below 1), then disable the button
       const prevButtonState = (action.payload.page - 1 < 1) ? true : false;
